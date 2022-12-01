25ed4221d8d675917ac40310fd68722d0c5c412e
Maria Paz Abal Obrien <paz.abal@globant.com>
2022-12-01 11:34:02 -0300

    new master

---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+const logotext = "PAZ";
+const meta = {
+    title: "Hi",
+    description: "Personal portfolio project developed with React",
+};
+
+const introdata = {
+    title: "I'm Paz Abal O'Brien",
+    animated: {
+        first: "I code cool websites ",
+        second: "I love React and NodeJS",
+        third: "Based in UY",
+    },
+    description: "Currently, I´m an employee at Globant as a Web UI Developer and Salesforce QA, looking for ways to put my knowledge and skills to use. I am a great communicator, motivated by any opportunity where I can learn new things or engage with people. ",
+    your_img_url: "https://i.im.ge/2022/08/31/OEJsaG.paz.jpg",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "You may notice a gap in my technical studies while reviewing my resume. During this period, I elected to start an English Teaching diploma in order to strengthen my soft skills. The time that I was able to commit to my soft skills education has been invaluable to my professional growth, and I am eager to apply my newfound qualifications and knowledge to a Full Stack role.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Web UI Developer",
+        where: "Globant (Bookings)",
+        date: "2022",
+    },
+    {
+        jobtitle: "Salesforce QA",
+        where: "Globant (HAPSA)",
+        date: "2022",
+    },
+    {
+        jobtitle: "Salesforce QA",
+        where: "Globant (Agrofy.SA)",
+        date: "2022",
+    },
+    {
+        jobtitle: "Game QA",
+        where: "Globant (Warner Bros. Games)",
+        date: "2021 - 2022",
+    },
+    {
+        jobtitle: "English Teacher",
+        where: "Fundación Sophia",
+        date: "2020",
+    },
+];
+
+const skills = [
+    // {
+    //     name: "Python",
+    //     value: 90,
+    // },
+    {
+        name: "NodeJS",
+        value: 65,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "PostgreSQL",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Frontend",
+        description: "Frontend web SPA creation with Javascript, HTML, CSS and/or React & Tailwind.",
+    },
+    {
+        title: "Backend",
+        description: "Server creation with Node & Express. Database creation with knex, PostgreSQL & PgAdmin.",
+    },
+    {
+        title: "Testing",
+        description: " Unit test creation with Jest & Postman for API testing. Version control with Git & Github ",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.im.ge/2022/09/15/1XBbx0.Capture.png",
+        desctiption: "Pokedex is an app where you can login, search and create different Pokémons. It was developed using React for the UI, NodeJS for the server, and PGAdmin for the database.",
+        link: "https://63221235156ffb32de3ff5b8--comforting-cocada-57d6cb.netlify.app/",
+        // link: "https://github.com/Pazabal/Pokedex-frontend",
+    },
+    {
+        img: "https://i.im.ge/2022/09/15/1XBbx0.Capture.png",
+        desctiption: "Pokedex Project Backend",
+        link: "https://github.com/Pazabal/Pokedex-backend",
+    },
+    {
+        img: "https://i.im.ge/2022/09/05/OR0kJX.calculator.png",
+        desctiption: "Javascript calculator app, with a history log ",
+        link: "https://63222120184cd54450494060--phenomenal-lamington-102df8.netlify.app/",
+    },
+    
+    {
+        img: "https://i.im.ge/2022/09/14/1XWthP.Capture.png",
+        desctiption: "This is a frontend website with a modern UI where you can find a header, a news section, articles, a contact form & a footer",
+        link: "https://63220b71ca134b2d7ecf60ad--cheerful-lily-4d2005.netlify.app/",
+    },
+    {
+        img: "https://i.im.ge/2022/09/05/ORm18Y.Capture.png",
+        desctiption: "In this netflix clone you can find the latest movies. Developed with react for frontend and netflix-unofficial rapid api to fetch the movies",
+        link: "https://github.com/Pazabal/netflix",
+    },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     desctiption: "Nexos is a modern website",
+    //     link: "https://github.com/Pazabal/nexos",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+   
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mariapaz.abal10@gmail.com",
+    YOUR_PHONE: "(598)91 622755",
+    description: "For more details about my background, please review my enclosed resume or feel free to contact me. I believe that I can be the Full Stack you're looking for and welcome the opportunity to speak with you at your earliest convenience.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_hqqjbbh",
+    YOUR_TEMPLATE_ID: "template_ab5j6zx",
+    YOUR_USER_ID: "xzxFypXC2GkaFfprT",
+    PRIVATE_KEY: "oTChnD2L4azuvD4hJ7eb1",
+};
+
+const socialprofils = {
+    github: "https://github.com/pazabal",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/pazabalobrien",
+    // resume: "https://i.im.ge/2022/09/05/ORPwhJ.AbalPaz-CV-WebUI-English-2022-1.jpg",
+    // youtube: "https://youtube.com",
+    // twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
